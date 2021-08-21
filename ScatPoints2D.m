bbbcf7ecd2200d7a85f3359faf5a219587c10cbc
ddmirzaei <89238155+ddmirzaei@users.noreply.github.com>
2021-08-21 23:26:10 +0430

    Add files via upload

--- ScatPoints2D.m
+++ ScatPoints2D.m
@@ -8,11 +8,11 @@ function [X,Xcov,PatchNearBound] = ScatPoints2D(a,b,h,hcov,PointType)
 % PointType: the type of point distribution, here 'grid' or 'halton' 
 switch (PointType)
   case ('grid')
-    [xt,yt]=meshgrid(a:h:b,a:h:b); X=[xt(:) yt(:)];                      
+    [xt,yt] = meshgrid(a:h:b,a:h:b); X = [xt(:) yt(:)];                      
   case('halton')
     N = round(((b-a)/h+1)^2); H = haltonset(2,'Skip',1e4,'Leap',1e2);
     X = net(H,N); X = (b-a)*X-a;
 end
-[xc,yc]=meshgrid(a:hcov:b,a:hcov:b);
-xc1=xc(:); xc2=yc(:); Xcov = [xc1 xc2];
+[xc,yc] = meshgrid(a:hcov:b,a:hcov:b);
+xc1 = xc(:); xc2 = yc(:); Xcov = [xc1 xc2];
 PatchNearBound = (xc1<a+hcov | xc1>b-hcov | xc2<a+hcov | xc2>b-hcov);
